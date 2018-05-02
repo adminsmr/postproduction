54f200fcbe23cc3ec2fe964ed66f0fae8ca47c59
Local Administrator <36914273+adminsmr@users.noreply.github.com>
2018-05-02 20:00:37 +0300

    Create delOld90DayFiles.sh
    File for remove old data from some path folder

---
+++ delOld90DayFiles.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# This script remove ALL FILES older 90 days in /mnt/storage/projects
+#
+
+timestamp=$(date +%Y%m%d_%H%M%S)
+path="/mnt/storage/projects/"
+filename=log_delolder90day_$timestamp.txt
+log=$path/$filename
+
+#find /mnt/storage/projects -mtime +90 -type f -print -delete >> $log
+
+echo "Delete:: Script Start -- $(date +%Y%m%d_%H%M)" >> $log
+START_TIME=$(date +%s)
+
+find /mnt/storage/projects -mtime +90 -type f -print -delete >> $log
+
+END_TIME=$(date +%s)
+ELAPSED_TIME=$(expr $END_TIME - $START_TIME)
+echo "Delete :: Script End -- $(date +%Y%m%d_%H%M)" >> $log
+echo "Elapsed Time ::  $(date -d $ELAPSED_TIME +%Hh:%Mm:%Ss) "  >> $log
+
+#exit 0
